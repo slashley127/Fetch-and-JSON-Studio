fe4231cbd47b362b63f7f23a9b0b6345acb64d3f
unknown <slashley127@gmail.com>
2022-07-07 19:15:00 -0500

    script edit

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // TODO: add code here
 
 window.addEventListener("load", function(){
-    let json = []
+    
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json){
-            const astronauts = document.getElementsByTagName("title");
+           console.log(json); 
             const container = document.getElementById("container");
-            let index = 0;
-            astronauts.addEventListener("click", function(){
-               container.innerHTML = `
-               <div class="astronaut">
-                    <div class ="bio">
-                        <h3>Name ${json[index].firstName} ${json[index].lastName}</h3>
+            for(astronaut of json){
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio"
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
                         <ul>
-                            <li>Hours in space: ${json[index].hoursInSpace}</li>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills}</li>
                         </ul>
                     </div>
+                    <img class="avatar" src=${astronaut.picture}>
                 </div>
                 `;
-                //index = (index+1) % json.length
+            }
                         
-            });
+            
         });
     });
 });
